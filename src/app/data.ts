2e01b8ac418848c088e223c7aa9f1747248d9ca8
carlosiib <ntic.carlos@gmail.com>
2025-06-02 11:38:26 -0700

    fix text spelling

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,20 +1,20 @@
 import { QuestionNode } from "./types";
 
-const addtional = [
+const additional = [
   {
     type: "l1",
     message:
-      "En el Formato Repartidor en la columna del cliente en el apartado de Notas indicarle cuantos cambios va a hacerle al cliente y escribirle una breve descripcion de lo platicado con el cliente. Agregar estos cambios en el borrador para contemplarlos con los pedidos de la ruta.",
+      "En el formato Repartidor, en la columna del cliente, en el apartado de Notas, indíquele cuántos cambios va a hacerle al cliente y escriba una breve descripción de lo platicado con él. Agregue estos cambios en el borrador para contemplarlos con los pedidos de la ruta.",
   },
   {
     type: "l4",
     message:
-      "Indicarle al repartidor que tiene que etiquetarle las bebidas al respectivo cliente. (Indicarle que recoja las bebidas al cliente y sutilmente etiquete las bebidas en la cabinatrasera del auto)",
+      "Indíquele al repartidor que debe etiquetar las bebidas del respectivo cliente. (Indíquele que recoja las bebidas del cliente y que, de manera sutil, las etiquete en la cabina trasera del auto).",
   },
   {
     type: "greet",
     message:
-      "Muchas gracias por su comprendimiento, le enviaremos al repartidor para realizarle la revision y los cambios necesarios . Estamos al pendientede cualquier comentario.",
+      "Muchas gracias por su comprensión. Le enviaremos al repartidor para realizar la revisión y los cambios necesarios. Estamos al pendiente de cualquier comentario.",
   },
 ];
 
@@ -26,19 +26,21 @@ export const QUESTIONS: QuestionNode[] = [
     answers: [
       {
         id: "q-01-a1",
-        question: "¿Cuantas son las bebidas que no tienen gas?",
+        question:
+          "¿Cuántas son las bebidas que no tienen gas?",
         answers: [
           {
             id: "q-01-a2",
-            message: "Esperar respuesta cliente",
+            message: "Esperar respuesta del cliente",
             question:
-              "Probablemente es que sea un problema con la boquilla, si gusta separar las bebidas para que no las venda, le enviaremos al repartidor para que les revise las bebidas y selas cambie.",
+              "Probablemente sea un problema con la boquilla. Si gusta separar las bebidas para que no las venda, le enviaremos al repartidor para que las revise y se las cambie.",
             answers: [
               {
                 id: "1-01-a2-a1",
                 isFinal: true,
-                question: addtional[0].message,
-                farewell: addtional[addtional.length - 1].message,
+                question: additional[0].message,
+                farewell:
+                  additional[additional.length - 1].message,
               },
             ],
           },
@@ -48,14 +50,14 @@ export const QUESTIONS: QuestionNode[] = [
   },
   {
     id: "q-02",
-    question: "question-02",
+    question: "Pregunta 02",
     type: "question",
     answers: [
       {
         id: "q-02-a2",
         isFinal: true,
-        question: "q-02-a2-text",
+        question: "Texto de q-02-a2",
       },
     ],
   },
-]
+];
